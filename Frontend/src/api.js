4312009c4b7c711eee19cd9b2ef6874c336f14fd
Ayush-1202 <ayushsingh93059@gmail.com>
2026-04-09 21:53:35 +0530

    link changed

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://compact-url.onrender.com",
+  baseURL: "https://compact-url.onrender.com/api/create",
 });
 
 export default api;
